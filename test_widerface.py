11df5cc64d860fc70c902625fa8454120e1ac858
AVargas1970 <alonso.vargas.e@ug.uchile.cl>
2021-01-12 01:18:57 -0300

    se agregan cambios para poder testear

--- test_widerface.py
+++ test_widerface.py
@@ -4,7 +4,7 @@ import argparse
 import torch
 import torch.backends.cudnn as cudnn
 import numpy as np
-from data import cfg_mnet, cfg_re50
+from data import cfg_mnet, cfg_re50, cfg_efb2, cfg_vov19
 from layers.functions.prior_box import PriorBox
 from utils.nms.py_cpu_nms import py_cpu_nms
 import cv2
@@ -14,9 +14,10 @@ from utils.timer import Timer
 
 
 parser = argparse.ArgumentParser(description='Retinaface')
-parser.add_argument('-m', '--trained_model', default='./weights/Resnet50_Final.pth',
-                    type=str, help='Trained state_dict file path to open')
-parser.add_argument('--network', default='resnet50', help='Backbone network mobile0.25 or resnet50')
+#parser.add_argument('-m', '--trained_model', default='./weights/Resnet50_Final.pth',
+#                    type=str, help='Trained state_dict file path to open')
+parser.add_argument('--network', default='efficientb2', help='Backbone network mobile0.25 or resnet50')
+parser.add_argument('--optimizer', default='sgd', help='optimizer for train, sgd, sgdp, or admap')
 parser.add_argument('--origin_size', default=True, type=str, help='Whether use origin image size to evaluate')
 parser.add_argument('--save_folder', default='./widerface_evaluate/widerface_txt/', type=str, help='Dir to save txt results')
 parser.add_argument('--cpu', action="store_true", default=False, help='Use cpu inference')
@@ -25,7 +26,7 @@ parser.add_argument('--confidence_threshold', default=0.02, type=float, help='co
 parser.add_argument('--top_k', default=5000, type=int, help='top_k')
 parser.add_argument('--nms_threshold', default=0.4, type=float, help='nms_threshold')
 parser.add_argument('--keep_top_k', default=750, type=int, help='keep_top_k')
-parser.add_argument('-s', '--save_image', action="store_true", default=False, help='show detection results')
+parser.add_argument('-s', '--save_image', action="store_true", default=True, help='show detection results')
 parser.add_argument('--vis_thres', default=0.5, type=float, help='visualization_threshold')
 args = parser.parse_args()
 
@@ -45,7 +46,7 @@ def check_keys(model, pretrained_state_dict):
 
 def remove_prefix(state_dict, prefix):
     ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
-    print('remove prefix \'{}\''.format(prefix))
+    #print('remove prefix \'{}\''.format(prefix))
     f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
     return {f(key): value for key, value in state_dict.items()}
 
@@ -55,6 +56,7 @@ def load_model(model, pretrained_path, load_to_cpu):
     if load_to_cpu:
         pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
     else:
+        print(torch.cuda.get_device_name(0))
         device = torch.cuda.current_device()
         pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
     if "state_dict" in pretrained_dict.keys():
@@ -74,12 +76,20 @@ if __name__ == '__main__':
         cfg = cfg_mnet
     elif args.network == "resnet50":
         cfg = cfg_re50
+    elif args.network == "efficientb2":
+        cfg = cfg_efb2
+    elif args.network == "vovnet19b":
+        cfg = cfg_vov19
+    else:
+        print("Invalid network!!")
+        raise ValueError
     # net and model
     net = RetinaFace(cfg=cfg, phase = 'test')
-    net = load_model(net, args.trained_model, args.cpu)
+    model_path = './weights/' + args.network + '_optim_' + args.optimizer + '_Final.pth'
+    net = load_model(net, model_path, args.cpu)
     net.eval()
     print('Finished loading model!')
-    print(net)
+    #print(net)
     cudnn.benchmark = True
     device = torch.device("cpu" if args.cpu else "cuda")
     net = net.to(device)
--- train.py
+++ train.py
@@ -31,7 +31,7 @@ args = parser.parse_args()
 
 if not os.path.exists(args.save_folder):
     os.mkdir(args.save_folder)
-cfg = None
+#cfg = None
 if args.network == "mobile0.25":
     cfg = cfg_mnet
 elif args.network == "resnet50":
@@ -40,12 +40,14 @@ elif args.network == "efficientb2":
     cfg = cfg_efb2
 elif args.network == "vovnet19b":
     cfg = cfg_vov19
-
+else:
+    print("Invalid network!!")
+    raise ValueError
 
 rgb_mean = (104, 117, 123) # bgr order
 num_classes = 2
 img_dim = cfg['image_size']
-num_gpu = cfg['ngpu']
+#num_gpu = cfg['ngpu']
 batch_size = cfg['batch_size']
 max_epoch = cfg['epoch']
 gpu_train = cfg['gpu_train']
@@ -82,7 +84,8 @@ if args.resume_net is not None:
         new_state_dict[name] = v
     net.load_state_dict(new_state_dict)
 
-if num_gpu >= 1 and gpu_train:
+#if num_gpu >= 1 and gpu_train:
+if gpu_train:
     net = torch.nn.DataParallel(net).cuda()
 else:
     net = net.cpu()
@@ -96,17 +99,20 @@ elif args.optimizer == 'sgdp':
     optimizer = SGDP(net.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, max_epoch)
 elif args.optimizer == 'adamp':
-    optimizer = AdamP(net.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
+    optimizer = AdamP(net.parameters(), lr=initial_lr, weight_decay=weight_decay)
+    scheduler = None
 else:
     print("Invalid optimizer!!")
     raise ValueError
 
+print("Optimizer : ", args.optimizer,"\n")
 criterion = MultiBoxLoss(num_classes, 0.35, True, 0, True, 7, 0.35, False, gpu_train)
 
 priorbox = PriorBox(cfg, image_size=(img_dim, img_dim))
 with torch.no_grad():
     priors = priorbox.forward()
-    if num_gpu >= 1 and gpu_train:
+    #if num_gpu >= 1 and gpu_train:
+    if gpu_train:
         priors = priors.cuda()
     else:
         priors = priors.cpu()
@@ -114,9 +120,11 @@ with torch.no_grad():
 def train():
     net.train()
     epoch = 0 + args.resume_epoch
-    print('Loading Dataset...')
-
+    info = '\rLoading Dataset...'
+    sys.stdout.write(info)
     dataset = WiderFaceDetection(training_dataset, preproc(img_dim, rgb_mean))
+    info += ' Dataset Ready.\n'
+    sys.stdout.write(info)
 
     epoch_size = math.ceil(len(dataset) / batch_size) # cantidad de batches
     max_iter = max_epoch * epoch_size
@@ -128,7 +136,7 @@ def train():
         start_iter = args.resume_epoch * epoch_size
     else:
         start_iter = 0
-
+    print("\nTraining Status :")
     for iteration in range(start_iter, max_iter):
         if iteration % epoch_size == 0:
             # create batch iterator
@@ -136,7 +144,11 @@ def train():
             if (epoch % 10 == 0 and epoch > 0): #or (epoch % 5 == 0 and epoch > cfg['decay1']):
                 torch.save(net.state_dict(), save_folder + cfg['name']+ '_optim_'+ args.optimizer +'_epoch_' + str(epoch) + '.pth')
             epoch += 1
-            if epoch > 1 and args.optimizer != 'adamp': scheduler.step() # cambia el lr
+            loss_l, loss_c, loss_landm, loss_tot = 0, 0, 0, 0
+            if epoch > 1:
+                if args.optimizer != 'adamp':
+                    scheduler.step() # cambia el lr
+                sys.stdout.write('\n')
 
         load_t0 = time.time()
         #if iteration in stepvalues:
@@ -147,7 +159,8 @@ def train():
 
         # load train data
         images, targets = next(batch_iterator)
-        if num_gpu >= 1 and gpu_train:
+        # if num_gpu >= 1 and gpu_train:
+        if gpu_train:
             images = images.cuda()
             targets = [anno.cuda() for anno in targets]
         else:
@@ -159,21 +172,27 @@ def train():
 
         # backprop
         optimizer.zero_grad()
-        loss_l, loss_c, loss_landm = criterion(out, priors, targets)
-        loss = cfg['loc_weight'] * loss_l + loss_c + loss_landm
+        #loss_l, loss_c, loss_landm = criterion(out, priors, targets)
+        multiTask_loss = criterion(out, priors, targets)
+        loss = cfg['loc_weight'] * multiTask_loss[0] + multiTask_loss[1] + multiTask_loss[2]
         loss.backward()
         optimizer.step()
-        load_t1 = time.time()
-        batch_time = load_t1 - load_t0
+
+        batch_time = time.time() - load_t0
         eta = int(batch_time * (max_iter - iteration))
+        items = iteration % epoch_size + 1
+        loss_l += multiTask_loss[0].item()
+        loss_c += multiTask_loss[1].item()
+        loss_landm += multiTask_loss[2].item()
+        loss_tot += loss.item()
         #print('Epoch:{}/{} || Epochiter: {}/{} || Iter: {}/{} || Loc: {:.4f} Cla: {:.4f} Landm: {:.4f} || LR: {:.8f} || Batchtime: {:.4f} s || ETA: {}'
         #      .format(epoch, max_epoch, (iteration % epoch_size) + 1,
         #      epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item(), lr, batch_time, str(datetime.timedelta(seconds=eta))))
-        info = f'\r7Epoch:{epoch}/{max_epoch} || '
+        info = f'\rEpoch:{epoch}/{max_epoch} || '
         info += f'Epochiter: {(iteration % epoch_size) + 1}/{epoch_size} || '
         info += f'Iter: {iteration + 1}/{max_iter} || '
-        info += f'Loc: {loss_l.item():.4f} Cla: {loss_c.item():.4f} Landm: {loss_landm.item():.4f} || '
-        info += f'LR: {lr:.8f} || Batchtime: {batch_time:.4f} s || ETA: {str(datetime.timedelta(seconds=eta))}'
+        info += f'Loc: {loss_l/items:.3f} Cla: {loss_c/items:.3f} Landm: {loss_landm/items:.3f} Loss: {loss_tot/items:.3f}|| '
+        info += f'LR: {lr:.8f} || Batchtime: {batch_time:.3f} s || ETA: {str(datetime.timedelta(seconds=eta))}'
         sys.stdout.write(info)
     torch.save(net.state_dict(), save_folder + cfg['name'] + '_optim_' + args.optimizer +'_Final.pth')
 
